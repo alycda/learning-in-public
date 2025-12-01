461ac1b111638f68ff7d28e6cbcfd1be5e316b97
alycda <alyda@me.com>
2025-11-30 16:24:07 -0800

    aoc: 2025 init rust

---
+++ advent-of-code/2025/rust/day-01/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn part1(_input: &str) -> Result<String, String> {
+    todo!("part 1!")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        todo!("write test")
+    }
+}
