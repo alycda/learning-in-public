855ccbd71ab8389d673376bf1a5c8bd8e7a86514
alycda <alyda@me.com>
2025-12-01 21:43:25 -0800

    2025-12-02: part 1

---
+++ advent-of-code/2025/rust/day-02/src/bin/part1.rs
@@ -0,0 +1,7 @@
+use day_02::part1;
+
+fn main() {
+    let file = include_str!("../../input.txt");
+    let result = part1(file);
+    println!("{}", result);
+}
---
+++ advent-of-code/2025/rust/day-02/src/lib.rs
@@ -0,0 +1,55 @@
+pub fn part1(input: &str) -> String {
+    let mut invalid_ids = vec![0];
+
+    input.lines().for_each(|line| {
+        line.split(',')
+            .for_each(|id| {
+                // dbg!(&id);
+                let mut ids = id.split('-');
+                let a = ids.next().unwrap().parse::<u64>().unwrap();
+                let b = ids.next().unwrap().parse::<u64>().unwrap();
+
+                // dbg!(a, b);
+
+                for i in a..=b {
+                    let s = i.to_string();
+                    let count = s.chars().count();
+
+                    if count % 2 == 0 {
+                        // dbg!(i);
+                        let mid = count / 2;
+                        let mut chars = s.chars();
+
+                        if chars.by_ref().take(mid).collect::<Vec<_>>() == chars.by_ref().take(mid).collect::<Vec<_>>() {
+                            // dbg!(i);
+                            invalid_ids.push(i);
+                        }
+                    }
+                }
+
+                // ids.for_each(|id| {
+                //     if let Ok(digits) = id.parse::<u64>() {
+                //         let mid = id.chars().count() / 2;
+                //         let mut chars = id.chars();
+
+                //         if chars.by_ref().take(mid).collect::<Vec<_>>() == chars.by_ref().take(mid).collect::<Vec<_>>() {
+                //             dbg!(digits);
+                //             invalid_ids.push(digits);
+                //         }
+                //     }
+                // });
+            });
+    });
+
+    invalid_ids.iter().sum::<u64>().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"), "1227775554".to_string());
+    }
+}
