3169437cbba53877649b030a4bbb1b120996e492
alycda <alyda@me.com>
2025-12-02 21:53:22 -0800

    2025-12-03: part 1

--- advent-of-code/2025/rust/day-03/src/lib.rs
+++ advent-of-code/2025/rust/day-03/src/lib.rs
@@ -1,20 +1,26 @@
-fn part1(input: &str) -> u32 {
+pub fn part1(input: &str) -> u32 {
     input.lines()
         .fold(vec![0], |mut acc, line| {
             let chars = line.chars();
             let char_count = line.chars().count();
 
-            dbg!(char_count);
+            // dbg!(char_count);
 
-            let nums = chars.map(|c| c.to_digit(10));
+            let mut nums = chars.map(|c| c.to_digit(10));
             let nums_clone = nums.clone();
             // dbg!(&nums);
 
+            // .rev() because if there are multiple maxes, the last one is returned. can't do this, breaks case #2
             let max = nums.take(char_count-1)
                 .enumerate()
-                .max_by_key(|(_, val)| *val)
+                .max_by(|a, b| {
+                    a.1.cmp(&b.1).then(std::cmp::Ordering::Greater)
+                })
+                // .max_by_key(|(_, val)| *val)
                 // .map(|(idx, _)| idx)
                 .unwrap();
+
+            // let first_max = nums.find(|e |e.unwrap() == max.1.unwrap());
 
             let max_idx = max.0;
             dbg!(max_idx);
@@ -53,12 +59,36 @@ fn part1(input: &str) -> u32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
 
-    #[test]
-    fn test_part1() {
-        assert_eq!(part1("987654321111111
-811111111111119
-234234234234278
-818181911112111"), 357);
+//     #[test]
+//     fn test_part1() {
+//         assert_eq!(part1("987654321111111
+// 811111111111119
+// 234234234234278
+// 818181911112111"), 357);
+//     }
+
+    #[rstest]
+    #[case("987654321111111", 98)]
+    #[case("811111111111119", 89)]
+    #[case("234234234234278", 78)]
+    #[case("818181911112111", 92)]
+    #[case("2232133233333122223222321121432322323324333234233221423334362333113343833132233313523312322224432234", 85)]
+    #[case("9544718948279477416294977734546287758964484675984344448638555429875995525496945633428322464129775449", 99)]
+    #[case("2342431222323242222322132121222212212132223222221322252221222222422212122222231227223222421241422222", 74)]
+    #[case("6244953925232293122334482643333513353336433435373235433333433333344373324258246634153623454355543453", 99)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    fn joltage(#[case] input: &str,#[case] expected: u32) {
+        assert_eq!(part1(input), expected);
     }
 }
