59c2f9e95c89849506cc693e9380bb2a3fbf1b0c
alycda <alyda@me.com>
2025-12-02 22:18:28 -0800

    2025-12-03: part 2 WIP

---
+++ advent-of-code/2025/rust/day-03/src/bin/part2.rs
@@ -0,0 +1,7 @@
+use day_03::part2;
+
+fn main() {
+    let file = include_str!("../../input.txt");
+    let result = part2(file);
+    println!("{result}");
+}
--- advent-of-code/2025/rust/day-03/src/lib.rs
+++ advent-of-code/2025/rust/day-03/src/lib.rs
@@ -4,53 +4,67 @@ pub fn part1(input: &str) -> u32 {
             let chars = line.chars();
             let char_count = line.chars().count();
 
-            // dbg!(char_count);
-
-            let mut nums = chars.map(|c| c.to_digit(10));
+            let nums = chars.map(|c| c.to_digit(10));
             let nums_clone = nums.clone();
-            // dbg!(&nums);
 
-            // .rev() because if there are multiple maxes, the last one is returned. can't do this, breaks case #2
             let max = nums.take(char_count-1)
                 .enumerate()
                 .max_by(|a, b| {
                     a.1.cmp(&b.1).then(std::cmp::Ordering::Greater)
                 })
-                // .max_by_key(|(_, val)| *val)
-                // .map(|(idx, _)| idx)
                 .unwrap();
-
-            // let first_max = nums.find(|e |e.unwrap() == max.1.unwrap());
 
             let max_idx = max.0;
             dbg!(max_idx);
 
-
             let next_max = nums_clone.skip(max_idx+1)
-                // .enumerate()
-                // .max_by_key(|(_, val)| *val)
-                // .map(|(idx, _)| idx)
                 .max()
                 .unwrap();
-            // dbg!(index_of_next_max + max_idx + 1);
 
             dbg!(max.1, next_max);
 
             acc.push(max.1.unwrap() * 10 + next_max.unwrap());
 
-            // acc.push(nums.nth(index_of_max.unwrap()).unwrap() * 10 + nums.nth(index_of_next_max.unwrap()).unwrap());
+            acc
+        }).iter().sum()
+}
 
-            // if index_of_max.unwrap() > line.chars().count() {
-            //     // return line.chars().skip(line.chars().count()-2).fold(0, |acc, c| acc * 10 + c.to_digit(10).unwrap_or(0))
-            // } 
+pub fn part2(input: &str) -> u64 {
+    input.lines()
+        .fold(vec![0], |mut acc, line| {
+            let chars = line.chars();
+            let char_count = line.chars().count();
 
-            // todo!("from rev find the highest number index, then compare to all following indices to find the next highest number");
+            let nums = chars.map(|c| c.to_digit(10));
+            // let _nums_clone = nums.clone();
 
-            // acc.push(line.chars().collect::<Vec<_>>().windows(2).map(|w| {
-            //     // dbg!(w);
+            let max = nums.take(char_count-12)
+                .enumerate()
+                .max_by(|a, b| {
+                    a.1.cmp(&b.1).then(std::cmp::Ordering::Greater)
+                })
+                .unwrap();
 
-            //     w.iter().fold(0, |acc, c| acc * 10 + c.to_digit(10).unwrap_or(0))
-            // }).max().unwrap());
+            let mut max_idx = max.0;
+            dbg!(max_idx);
+            let mut joltage = vec![max.1.unwrap()];
+
+            for _ in 0..11 {
+                let nums_clone = line.chars().map(|c| c.to_digit(10));
+                let next_max = nums_clone.skip(max_idx+1)
+                    .enumerate()
+                    .max_by(|a, b| {
+                        a.1.cmp(&b.1).then(std::cmp::Ordering::Greater)
+                    })
+                    .unwrap();
+
+                max_idx = next_max.0 + max_idx + 1;
+                joltage.push(next_max.1.unwrap());
+            }
+
+            // dbg!(&joltage);
+
+            acc.push(joltage.iter().map(|d| char::from_digit(*d, 10).unwrap()).collect::<String>().parse::<u64>().unwrap());
 
             acc
         }).iter().sum()
@@ -61,13 +75,13 @@ mod tests {
     use super::*;
     use rstest::rstest;
 
-//     #[test]
-//     fn test_part1() {
-//         assert_eq!(part1("987654321111111
-// 811111111111119
-// 234234234234278
-// 818181911112111"), 357);
-//     }
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("987654321111111
+811111111111119
+234234234234278
+818181911112111"), 357);
+    }
 
     #[rstest]
     #[case("987654321111111", 98)]
@@ -78,17 +92,31 @@ mod tests {
     #[case("9544718948279477416294977734546287758964484675984344448638555429875995525496945633428322464129775449", 99)]
     #[case("2342431222323242222322132121222212212132223222221322252221222222422212122222231227223222421241422222", 74)]
     #[case("6244953925232293122334482643333513353336433435373235433333433333344373324258246634153623454355543453", 99)]
-    // #[case("", 0)]
-    // #[case("", 0)]
-    // #[case("", 0)]
-    // #[case("", 0)]
-    // #[case("", 0)]
-    // #[case("", 0)]
-    // #[case("", 0)]
-    // #[case("", 0)]
-    // #[case("", 0)]
-    // #[case("", 0)]
-    fn joltage(#[case] input: &str,#[case] expected: u32) {
+    fn joltage_2(#[case] input: &str,#[case] expected: u32) {
         assert_eq!(part1(input), expected);
     }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("987654321111111
+811111111111119
+234234234234278
+818181911112111"), 3121910778619);
+    }
+
+    #[rstest]
+    #[case("987654321111111", 987654321111)]
+    #[case("811111111111119", 811111111119)]
+    #[case("234234234234278", 434234234278)]
+    #[case("818181911112111", 888911112111)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    // #[case("", 0)]
+    fn joltage_12(#[case] input: &str,#[case] expected: u64) {
+        assert_eq!(part2(input), expected);
+    }
+
 }
