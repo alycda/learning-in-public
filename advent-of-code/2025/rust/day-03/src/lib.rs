f46ec04f94d9d321ded2c123867c43dfb6fd1536
alycda <alyda@me.com>
2025-12-02 21:37:05 -0800

    2025-12-03: part 1 WIP

--- advent-of-code/2025/rust/day-03/src/lib.rs
+++ advent-of-code/2025/rust/day-03/src/lib.rs
@@ -1,15 +1,52 @@
 fn part1(input: &str) -> u32 {
     input.lines()
         .fold(vec![0], |mut acc, line| {
-            // dbg!(line.chars());
+            let chars = line.chars();
+            let char_count = line.chars().count();
 
-            acc.push(line.chars().collect::<Vec<_>>().windows(2).map(|w| {
-                // dbg!(w);
+            dbg!(char_count);
 
-                w.iter().fold(0, |acc, c| acc * 10 + c.to_digit(10).unwrap_or(0))
-            }).max().unwrap());
+            let nums = chars.map(|c| c.to_digit(10));
+            let nums_clone = nums.clone();
+            // dbg!(&nums);
 
-            dbg!(acc)
+            let max = nums.take(char_count-1)
+                .enumerate()
+                .max_by_key(|(_, val)| *val)
+                // .map(|(idx, _)| idx)
+                .unwrap();
+
+            let max_idx = max.0;
+            dbg!(max_idx);
+
+
+            let next_max = nums_clone.skip(max_idx+1)
+                // .enumerate()
+                // .max_by_key(|(_, val)| *val)
+                // .map(|(idx, _)| idx)
+                .max()
+                .unwrap();
+            // dbg!(index_of_next_max + max_idx + 1);
+
+            dbg!(max.1, next_max);
+
+            acc.push(max.1.unwrap() * 10 + next_max.unwrap());
+
+            // acc.push(nums.nth(index_of_max.unwrap()).unwrap() * 10 + nums.nth(index_of_next_max.unwrap()).unwrap());
+
+            // if index_of_max.unwrap() > line.chars().count() {
+            //     // return line.chars().skip(line.chars().count()-2).fold(0, |acc, c| acc * 10 + c.to_digit(10).unwrap_or(0))
+            // } 
+
+            // todo!("from rev find the highest number index, then compare to all following indices to find the next highest number");
+
+            // acc.push(line.chars().collect::<Vec<_>>().windows(2).map(|w| {
+            //     // dbg!(w);
+
+            //     w.iter().fold(0, |acc, c| acc * 10 + c.to_digit(10).unwrap_or(0))
+            // }).max().unwrap());
+
+            acc
         }).iter().sum()
 }
 
