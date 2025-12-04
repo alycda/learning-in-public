bf5e3f87d1e34ad5b779929e16742bcdf84e7054
alycda <alyda@me.com>
2025-12-03 21:50:42 -0800

    fix ci

--- advent-of-code/2025/rust/day-04/src/bin/part2.rs
+++ advent-of-code/2025/rust/day-04/src/bin/part2.rs
@@ -4,6 +4,10 @@ use aoc_ornaments::spatial::Grid;
 use day_04::part2;
 
 fn main() {
+    #[cfg(feature = "ci")]
+    let file = day_04::SAMPLE_INPUT;
+
+    #[cfg(not(feature = "ci"))]
     let file = include_str!("../../input.txt");
 
     let grid = Grid::<char>::from_str(file).unwrap();
