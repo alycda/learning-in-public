d6a2f7f73eabe98cc511791bc411ca0114374e74
alycda <alyda@me.com>
2025-12-06 22:38:12 -0800

    2025-12-07 part 2 WIP

--- advent-of-code/2025/rust/day-07/src/lib.rs
+++ advent-of-code/2025/rust/day-07/src/lib.rs
@@ -1,27 +1,13 @@
-use std::{collections::HashSet, str::FromStr};
+use std::{collections::{HashMap, HashSet}, str::FromStr};
 
 use aoc_ornaments::spatial::{Grid, Position};
 
 pub fn part1(input: &str) -> usize {
     let start = input.lines().next().unwrap().chars().enumerate().find(|(_idx, c)| *c == 'S').unwrap();
-    // let mut beams = vec![(0, start.0)];
-
-    // let mut beams = HashSet::<usize>::new();
-    // beams.insert(start.0);
-
-    // let mut splits = HashSet::new();
-
-    // // dbg!(start.0, &beams);
 
     let mut splits_hit = HashSet::new();
     let mut active_cols = HashSet::from([start.0]);
     let grid = Grid::<char>::from_str(input).unwrap();
-
-    // grid.walk(|pos| {
-
-    // });
-
-    // // dbg!(&grid);
 
     for row in 1..grid.get_height() {
         let mut next_cols = HashSet::new();
@@ -42,38 +28,48 @@ pub fn part1(input: &str) -> usize {
         }
 
         active_cols = next_cols;
-
-    //     dbg!(c);
-
-    //     match c {
-    //         '^' => {
-    //             // remove the beam from the previous row and replace it with 2 new beams
-    //             beams.remove(&7);
-    //             splits.insert(7);
-    //             beams.insert(6);
-    //             beams.insert(8);
-    //         }
-    //         _ => {}
-    //     }
     };
-
-
-
-    // // input.lines().map(|line| {
-    // //     line.chars().fold(0, |mut acc, c| {
-    // //         if c == '^' {
-    // //             acc +=1;
-    // //         }
-
-    // //         acc
-    // //     })
-    // // }).sum::<usize>() - 1
 
     splits_hit.len()
 }
 
+fn count_paths(grid: &Grid<char>, row: usize, col: usize, memo: &mut HashMap<(usize, usize), usize>) -> usize {
+    if row > grid.get_height() {
+        return 1;
+    }
+
+    if let Some(&cached) = memo.get(&(row, col)) {
+        return cached;
+    }
+
+    let result = match grid.get_at_unbounded(Position::new(row as i32, col as i32)) {
+        '^' => {
+            let left = if col > 0 {
+                count_paths(grid, row + 1, col - 1, memo)
+            } else {
+                0
+            };
+            let right = if col + 1 < grid.get_height() {
+                count_paths(grid, row + 1, col + 1, memo)
+            } else {
+                0
+            };
+            left + right
+        }
+        _ => count_paths(grid, row + 1, col, memo)
+    };
+
+    memo.insert((row, col), result);
+    result
+}
+
 pub fn part2(input: &str) -> usize {
-    todo!()
+    let mut memo = HashMap::new();
+    let grid = Grid::<char>::from_str(input).unwrap();
+    let start = input.lines().next().unwrap().chars().enumerate().find(|(_idx, c)| *c == 'S').unwrap();
+    memo.insert((0, start.0), 0);
+
+    count_paths(&grid, 0, 1, &mut memo)
 }
 
 pub const SAMPLE_INPUT: &str = ".......S.......
@@ -96,38 +92,14 @@ pub const SAMPLE_INPUT: &str = ".......S.......
 #[cfg(test)]
 mod tests {
     use super::*;
-//     use rstest::rstest;
-
-//     #[rstest]
-//     #[case(".......S.......", 0)]
-//     #[case(".......S.......
-// ...............", 0)]
-//     #[case(".......S.......
-// ...............
-// .......^.......", 1)]
-//     #[case(".......S.......
-// .......|.......
-// ......|^|......
-// ......|.|......
-// ......^.^......", 3)]
-//     // #[case("...", 0)]
-//     // #[case("...", 0)]
-//     // #[case("...", 0)]
-//     // #[case("...", 0)]
-//     // #[case("...", 0)]
-//     // #[case("...", 0)]
-//     // #[case("...", 0)]
-//     fn test_cases(#[case] input: &str, #[case] expected: usize) {
-//         assert_eq!(part1(input), expected);
-//     }
 
     #[test]
     fn test_part1() {
         assert_eq!(part1(SAMPLE_INPUT), 21);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(SAMPLE_INPUT), 0);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(SAMPLE_INPUT), 40);
+    }
 }
